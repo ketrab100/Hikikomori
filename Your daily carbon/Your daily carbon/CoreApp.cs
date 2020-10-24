c65bd912958d6a29976854547abd223daeec7d8e
SuperCiuper <63669112+SuperCiuper@users.noreply.github.com>
2020-10-24 19:38:04 +0200

    dodawanie i odczytywanie

--- Your daily carbon/Your daily carbon/CoreApp.cs
+++ Your daily carbon/Your daily carbon/CoreApp.cs
@@ -19,14 +19,47 @@ namespace Your_daily_carbon
         {
             string print = "    Currently you have " + dataStorage.listOfVehicles.Count() + "means of transport added: \n";
             //wypisz linię informującą o ilości różnych środków transportu, potem je wypisuje po kolei ze spalaniem na g/km
-            print += "    Vehicle name    |   CO2emission [g/km]";
+            print += "    Vehicle name            |    CO2emission [g/km]";
 
             for(int q=0; q<dataStorage.listOfVehicles.Count(); q++)
             {
                 print += "    ";
-                print += dataStorage.listOfVehicles(q);
+                print += makeWordFit(dataStorage.listOfVehicles[q].name, 24);
+                print += Convert.ToString(dataStorage.listOfVehicles[q].co2) + "\n";
             }
             return print;
+        }
+
+        public void addProduct(string name, string companyName, string weight, string co2)
+        {
+            dataStorage.listofProducts.Add(new Product(name, companyName, Convert.ToInt32(weight), Convert.ToInt32(co2)));
+        }
+
+        public string showProducts()
+        {
+            string print = "    Currently you have " + dataStorage.listofProducts.Count() + "means of transport added: \n";
+            //wypisz linię informującą o ilości różnych środków transportu, potem je wypisuje po kolei ze spalaniem na g/km
+            print += "    Product name            |    Company name            |    Weight [g]    |    CO2emission [g/km]";
+
+            for (int q = 0; q < dataStorage.listOfVehicles.Count(); q++)
+            {
+                print += "    ";
+                print+=makeWordFit(dataStorage.listofProducts[q].name, 24);
+                print += makeWordFit(dataStorage.listofProducts[q].companyName, 24);
+                print += makeWordFit(Convert.ToString(dataStorage.listofProducts[q].weight), 14);
+                print += Convert.ToString(dataStorage.listofProducts[q].co2) + "\n";
+            }
+            return print;
+        }
+
+        private string makeWordFit(string word, int howLong)
+        {
+            while(word.Length!=howLong)
+            {
+                word += " ";
+            }
+            word += "|    ";
+            return word;
         }
     }
 }
--- Your daily carbon/Your daily carbon/CurrentDataStorage.cs
+++ Your daily carbon/Your daily carbon/CurrentDataStorage.cs
@@ -10,10 +10,9 @@ namespace Your_daily_carbon
     {
 
         internal List<Vehicle> listOfVehicles = new List<Vehicle>();
+        internal List<Product> listofProducts = new List<Product>();
 
-        Vehicle xd = new Vehicle("as", 200);
 
-        string test = xd.name;
     }
 
     class Vehicle
@@ -24,10 +23,47 @@ namespace Your_daily_carbon
             this.name = name;
             this.co2 = co2;
         }
-        public string name;
+        public string name
+        {
+            get;
+            private set;
+        }
 
-        private int co2;
+        public float co2
+        {
+            get;
+            private set;
+        }
+    }
 
-        
+    class Product
+    {
+        public Product(string name, string companyName, int weight, co2)
+        {
+            this.name = name;
+            this.companyName = companyName;
+            this.weight = weight;
+            this.co2 = co2;
+        }
+        public string name
+        {
+            get;
+            private set;
+        }
+        public string companyName
+        {
+            get;
+            private set;
+        }
+        public int weight
+        {
+            get;
+            private set;
+        }
+        public float co2
+        {
+            get;
+            private set;
+        }
     }
 }
