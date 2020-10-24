f619b3a0311f92681041326d973205167d706764
ketrab100 <bartis300@gmail.com>
2020-10-24 18:13:04 +0200

    barcode reader

--- Your daily carbon/Your daily carbon/Form1.cs
+++ Your daily carbon/Your daily carbon/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ZXing;
 
 namespace Your_daily_carbon
 {
@@ -15,6 +16,31 @@ namespace Your_daily_carbon
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = ofd.FileName;
+                BarcodeReader reader = new BarcodeReader();
+                Bitmap bitmap = new Bitmap(filePath);
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom; 
+                pictureBox1.Image = bitmap;
+                var result = reader.Decode((Bitmap)bitmap);
+                if (result != null)
+                {
+                    textBox1.Text = result.ToString();
+                }
+                
+            }
+            
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
