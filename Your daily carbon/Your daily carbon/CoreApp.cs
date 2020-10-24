d2c4f2f2c6cfe0631259de027ab67ad79b0e2b05
SuperCiuper <63669112+SuperCiuper@users.noreply.github.com>
2020-10-24 18:48:30 +0200

    get set

---
+++ Your daily carbon/Your daily carbon/CoreApp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Your_daily_carbon
+{
+    class CoreApp
+    {
+        CurrentDataStorage dataStorage = new CurrentDataStorage();
+
+        public void addTransportMethod(string name, string co2)
+        {
+            dataStorage.listOfVehicles.Add(new Vehicle(name, Convert.ToInt32(co2)));
+        }
+
+        public string showTransportMethods()
+        {
+            string print = "    Currently you have " + dataStorage.listOfVehicles.Count() + "means of transport added: \n";
+            //wypisz linię informującą o ilości różnych środków transportu, potem je wypisuje po kolei ze spalaniem na g/km
+            print += "    Vehicle name    |   CO2emission [g/km]";
+
+            for(int q=0; q<dataStorage.listOfVehicles.Count(); q++)
+            {
+                print += "    ";
+                print += dataStorage.listOfVehicles(q);
+            }
+            return print;
+        }
+    }
+}
---
+++ Your daily carbon/Your daily carbon/CurrentDataStorage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Your_daily_carbon
+{
+    class CurrentDataStorage
+    {
+
+        internal List<Vehicle> listOfVehicles = new List<Vehicle>();
+
+        Vehicle xd = new Vehicle("as", 200);
+
+        string test = xd.name;
+    }
+
+    class Vehicle
+    {
+
+        public Vehicle(string name, int co2)
+        {
+            this.name = name;
+            this.co2 = co2;
+        }
+        public string name;
+
+        private int co2;
+
+        
+    }
+}
